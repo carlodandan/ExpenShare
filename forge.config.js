81154e26900e5d76707cf26e4791078c216d2eb8
Carlo Dandan <carlodandan.personal@proton.me>
2026-08-16 03:15:17 +0800

    Update: Made sure to use only squirrel
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- forge.config.js
+++ forge.config.js
@@ -19,19 +19,9 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin', 'linux', 'win32'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        name: 'Budget Tracker',
+      },
     },
   ],
   publishers: [
